0fda8e77db657ed2f980b118c29826906830b861
HojiakbarSultonov <hojiakbarrasulovich@gmail.com>
2023-07-25 21:09:31 +0500

    variable

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+$rang = 'Hojiakbar';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mening php filem</title>
+</head>
+<body>
+    <div>
+        salom
+    </div>
+
+    <div>
+        <?php 
+            echo $rang
+        ?>
+    </div>
+
+    
+</body>
+</html>
+
